6aede665e5d2b0b668f14466fe11af2caea602c3
pjyy2k <deneb.jin@gmail.com>
2015-12-14 15:49:55 +0900

    데이터 전처리

--- main.R
+++ main.R
@@ -1,5 +1,17 @@
 install.packages("dplyr")
 library(dplyr)
 
-data <- read.table(file="./DataSource/train.csv",header = TRUE, sep = ",")
-data
+bikedata <- read.table(file="./DataSource/train.csv",header = TRUE, sep = ",")
+bikedata
+#자료 Summary 관찰.
+str(bikedata) #자료 형태 관찰
+head(bikedata) #자료 미리보기
+summary(bikedata) #자료 요약값 제공
+
+cor(bikedata)
+
+# REF, 데이터 시각화 하는 방법
+library(corrplot)
+mcor <- cor(mtcars)
+round(mcor, 2) 
+corrplot(mcor, method='shade', shade.col=NA, tl.col='black', tl.srt=45)
