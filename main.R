61e88188733eb040b6a20555ecfbbb12271cfe79
pjyy2k <deneb.jin@gmail.com>
2015-12-14 16:47:36 +0900

    Overview1

--- main.R
+++ main.R
@@ -7,9 +7,25 @@ bikedata
 str(bikedata) #자료 형태 관찰
 head(bikedata) #자료 미리보기
 summary(bikedata) #자료 요약값 제공
+plot(bikedata) # Scotter plot
 
-cor(bikedata)
 
+#Datatime 연도별, 월별, 일별, 요일별, 시간별로 바꿀 수 있을 듯, 시간대도 가능하면 투입. 오전, 오후, 저녁, 심야, 새벽 등으로 구분
+bikedata$season <- factor(bikedata$season)
+str(bikedata)
+
+attach(bikedata)
+
+
+bikemodel.1<-lm(count~season+weather+temp) # 변경예정
+
+#season 같은 경우 순서형 변수가 아니므로 명목형태로 바꾸어 줘야 할듯.
+#weather는 Ranking을 가진다고 볼 수 있을 것 같음 클수록 악천후
+
+
+summary(bikemodel.1)
+attributes(bikemodel.1)
+plot(count,fitted.values(bikemodel.1))
 # REF, 데이터 시각화 하는 방법
 library(corrplot)
 mcor <- cor(mtcars)
