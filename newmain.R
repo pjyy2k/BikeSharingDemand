74497b5c04902eb3061920ec51fc2008eb1acab6
pjyy2k <deneb.jin@gmail.com>
2015-12-18 23:16:50 +0900

    222

--- newmain.R
+++ newmain.R
@@ -39,39 +39,94 @@ df$weekday[df$weekday=="목요일"]<-5
 df$weekday[df$weekday=="금요일"]<-6
 df$weekday[df$weekday=="토요일"]<-7
 
+#자전거 대여량 범주화
+for(i in 0:4){ # bike 대여 count에 따라 범주화(200단위씩 끊어서)
+  df$CatCount[df$count >= i*200 & df$count < (i+1)*200] <- (i)
+}
+rm(i) #불필요한 변수 삭제
+df$dmfcc =1 #dummy for cell counting. all has '1'
+str(df)
 
 #분할표 작성
-
 #분할표내에서 사용할 변수선언
 weekday<-numeric(0)
 hour <-numeric(0)
 weather <-numeric(0)
 months<-numeric(0)
-count <-numeric(0)
-
+frq1 <-numeric(0)
+frq2 <-numeric(0)
+frq3 <-numeric(0)
+frq4 <-numeric(0)
+frq5 <-numeric(0)
 #일부 변수는 제외 : workingday -> holiday로 대체, season -> months로 대체
 #추후 선택해야 하는 변수 : holiday & weekday => holiday중 weekday가 차지하는 비중이 높아 다중공선성의 문제가 발생할 여지가 있기 때문에 선택예정
 #
-
 for (h in 1:7){ # of weekday
-for (i in 1:4){ # of weather
-  for(j in 1:24){ # of hour
-    for(k in 1:12){ # of months
-      weekday<-c(weekday,h)
-      weather<-c(weather,i)
-      hour<-c(hour,j)
-      months<-c(months,k)
-      count<-c(count,sum(df$count[df$weekday==h & df$weather==i & df$hour==j & df$months==k]))
+  for (i in 1:4){ # of weather
+    for(j in 1:24){ # of hour
+      for(k in 1:12){ # of months
+        weekday<-c(weekday,h)
+        weather<-c(weather,i)
+        hour<-c(hour,j)
+        months<-c(months,k)
+        frq1<-c(frq1,sum(df$dmfcc[df$weekday==h & df$weather==i & df$hour==j & df$months==k & df$CatCount==0]))
+        frq2<-c(frq2,sum(df$dmfcc[df$weekday==h & df$weather==i & df$hour==j & df$months==k & df$CatCount==1]))
+        frq3<-c(frq3,sum(df$dmfcc[df$weekday==h & df$weather==i & df$hour==j & df$months==k & df$CatCount==2]))
+        frq4<-c(frq4,sum(df$dmfcc[df$weekday==h & df$weather==i & df$hour==j & df$months==k & df$CatCount==3]))
+        frq5<-c(frq5,sum(df$dmfcc[df$weekday==h & df$weather==i & df$hour==j & df$months==k & df$CatCount==4]))
       }
     }
   }
 }
-ctgctable<-data.frame(weekday,weather,hour,months,count)
-rm(weekday,weather,hour,months,count,h,i,j,k)
+
+hour <-numeric(0)
+frq1 <-numeric(0)
+frq2 <-numeric(0)
+frq3 <-numeric(0)
+frq4 <-numeric(0)
+frq5 <-numeric(0)
+for(j in 1:24){ # of hour
+  hour<-c(hour,j)
+  frq1<-c(frq1,sum(df$dmfcc[df$hour==j & df$CatCount==0]))
+  frq2<-c(frq2,sum(df$dmfcc[df$hour==j & df$CatCount==1]))
+  frq3<-c(frq3,sum(df$dmfcc[df$hour==j & df$CatCount==2]))
+  frq4<-c(frq4,sum(df$dmfcc[df$hour==j & df$CatCount==3]))
+  frq5<-c(frq5,sum(df$dmfcc[df$hour==j & df$CatCount==4]))
+}
+table.hour<-data.frame(hour,frq1,frq2,frq3,frq4,frq5)
+
+
+
+
+
+
+ctgctable<-data.frame(weekday,weather,hour,months,frq1,frq2,frq3,frq4,frq5)
+rm(weekday,weather,hour,months,count,CatCount,frq1,frq2,frq3,frq4,frq5,g,h,i,j,k,l)
+
+
+ctgctable$weekday<-as.factor(ctgctable$weekday)
+ctgctable$weather<-as.integer(ctgctable$weather)
+ctgctable$hour<-as.factor(ctgctable$hour)
+ctgctable$months<-as.factor(ctgctable$months)
+#ctgctable$holiday<-as.factor(ctgctable$holiday)
+ctgctable$frq1<-as.integer(ctgctable$frq1)
+ctgctable$frq2<-as.integer(ctgctable$frq2)
+ctgctable$frq3<-as.integer(ctgctable$frq3)
+ctgctable$frq4<-as.integer(ctgctable$frq4)
+ctgctable$frq5<-as.integer(ctgctable$frq5)
+
+str(ctgctable)
+
+
+
 
 #model fitting
 
-ftd.model0 <- glm(count~weekday+weather+hour+months,data=ctgctable)
+ftd.model0 <- glm(CatCount~weekday*weather*hour*months,data=ctgctable)
+install.packages("VGAM")
+library(VGAM)
+ftd.model.hour <-vglm()
+
 summary(ftd.model0)
 
 #한계점 메모 :
